0090d148902e89545569060917cbc082509445d0
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-04 19:23:19 +0000

    feat(PassHasher): Add initial passwordHasher

---
+++ Controllers/PasswordHasherController.cs
@@ -0,0 +1,37 @@
+﻿namespace BooksApi.Controllers
+{
+    using System;
+    using System.Security.Cryptography;
+    using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+    using Microsoft.AspNetCore.Mvc;
+
+    // https://github.com/aspnet/Identity/blob/master/src/Core/PasswordHasher.cs
+    // https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-2.2
+
+    [Route("api/[controller]")]
+    public class PasswordHasherController : ControllerBase
+    {
+        [HttpGet("{password}")]
+        public ActionResult<string> Get(string password)
+        {
+            // generate a 128-bit salt using a secure PRNG
+            var salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            Console.WriteLine($"Hashed: {hashed}");
+
+            return hashed ?? (ActionResult<string>)this.NotFound();
+        }
+    }
+}
