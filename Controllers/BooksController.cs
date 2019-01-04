c5f11ec2ea7e86c6c1bd1b65d0271b1e3421cdcf
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-04 09:31:55 +0000

    feat(Boostrap): Initial commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,72 @@
+﻿namespace BooksApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using BooksApi.Models;
+    using BooksApi.Services;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService bookService;
+
+        public BooksController(BookService bookService)
+        {
+            this.bookService = bookService ?? throw new ArgumentNullException(nameof(bookService));
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            return this.bookService.Get();
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = this.bookService.Get(id);
+
+            return book ?? (ActionResult<Book>) this.NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            this.bookService.Create(book);
+
+            return this.CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = this.bookService.Get(id);
+
+            if (book == null)
+            {
+                return this.NotFound();
+            }
+
+            this.bookService.Update(id, bookIn);
+
+            return this.NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = this.bookService.Get(id);
+
+            if (book == null)
+            {
+                return this.NotFound();
+            }
+
+            this.bookService.Remove(book.Id);
+
+            return this.NoContent();
+        }
+    }
+}
---
+++ Services/BookService.cs
@@ -0,0 +1,55 @@
+﻿namespace BooksApi.Services
+{
+    using System.Collections.Generic;
+    using BooksApi.Models;
+    using Microsoft.Extensions.Configuration;
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> books;
+
+        public BookService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("BookstoreDb"));
+            var database = client.GetDatabase("BookstoreDb");
+            this.books = database.GetCollection<Book>("Books");
+        }
+
+        public List<Book> Get()
+        {
+            return this.books.Find(book => true).ToList();
+        }
+
+        public Book Get(string id)
+        {
+            var docId = new ObjectId(id);
+
+            return this.books.Find<Book>(book => book.Id == docId).FirstOrDefault();
+        }
+
+        public Book Create(Book book)
+        {
+            this.books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn)
+        {
+            var docId = new ObjectId(id);
+
+            this.books.ReplaceOne(book => book.Id == docId, bookIn);
+        }
+
+        public void Remove(Book bookIn)
+        {
+            this.books.DeleteOne(book => book.Id == bookIn.Id);
+        }
+
+        public void Remove(ObjectId id)
+        {
+            this.books.DeleteOne(book => book.Id == id);
+        }
+    }
+}
